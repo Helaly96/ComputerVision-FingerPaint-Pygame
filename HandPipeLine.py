f53c8051baa33401c9983acf6c37c231f5698eb6
Helaly96 <zfaesu@gmail.com>
2020-12-17 15:49:01 +0200

    added hand-drawing with mediapipe

--- HandPipeLine.py
+++ HandPipeLine.py
@@ -1,12 +1,15 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-
+ImageCounter= 0
 # For webcam input:
 hands = mp_hands.Hands(
     min_detection_confidence=0.5, min_tracking_confidence=0.5)
 cap = cv2.VideoCapture(0)
+PointsToDraw= []
 while cap.isOpened():
   success, image = cap.read()
   if not success:
@@ -17,6 +20,8 @@ while cap.isOpened():
   # Flip the image horizontally for a later selfie-view display, and convert
   # the BGR image to RGB.
   image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+  TempImage= np.zeros((image.shape)).astype('float32')
+
   H,W,_ = image.shape
   # To improve performance, optionally mark the image as not writeable to
   # pass by reference.
@@ -30,12 +35,32 @@ while cap.isOpened():
       XPixel_IndexFinger= hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * W
       YPixel_IndexFinger= hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * H
 
-      XPixel
-      cv2.circle(image, (int(XPixel),int(YPixel)), 20, (255, 0, 0), -1)
+      XPixel_MiddleFinger= hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * W
+      YPixel_MiddleFinger= hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * H
+
+      cv2.circle(image, (int(XPixel_IndexFinger),int(YPixel_IndexFinger)), 20, (255, 0, 0), -1)
+      cv2.circle(image, (int(XPixel_MiddleFinger),int(YPixel_MiddleFinger)), 20, (0, 0, 255), -1)
+      dist= abs(np.linalg.norm(np.array([XPixel_IndexFinger,YPixel_IndexFinger])-np.array([XPixel_MiddleFinger,YPixel_MiddleFinger])))
+
+      if (dist<90):
+        PointsToDraw.append([int(XPixel_MiddleFinger),int(YPixel_MiddleFinger)])
+      else:
+        PointsToDraw=[]
+
+      for i in range(len(PointsToDraw)):
+        cv2.circle(TempImage, tuple(PointsToDraw[i]), 20, (255, 0, 0), -1)
+
+      # print("Distance between the 2 fingers"+str(dist))
       mp_drawing.draw_landmarks(
           image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
   cv2.imshow('MediaPipe Hands', image)
-  if cv2.waitKey(5) & 0xFF == 27:
+  cv2.imshow('Canvas', TempImage)
+  k= cv2.waitKey(27)
+  if k & 0xFF == 27:
     break
+  elif k == ord('e'):
+    cv2.imwrite(str(ImageCounter)+".jpg",TempImage)
+    print("!!")
+
 hands.close()
 cap.release()
