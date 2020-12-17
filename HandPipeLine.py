75fa0ab37675e1797703641e9109e6ac48a771b6
Helaly96 <zfaesu@gmail.com>
2020-12-17 14:53:17 +0200

    Modified the Image Processing Script, working on the HandPipeline Starts

--- HandPipeLine.py
+++ HandPipeLine.py
@@ -17,16 +17,19 @@ while cap.isOpened():
   # Flip the image horizontally for a later selfie-view display, and convert
   # the BGR image to RGB.
   image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+  H,W,_ = image.shape
   # To improve performance, optionally mark the image as not writeable to
   # pass by reference.
   image.flags.writeable = False
   results = hands.process(image)
-
   # Draw the hand annotations on the image.
   image.flags.writeable = True
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
   if results.multi_hand_landmarks:
     for hand_landmarks in results.multi_hand_landmarks:
+      XPixel= hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * W
+      YPixel= hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * H
+      cv2.circle(image, (int(XPixel),int(YPixel)), 20, (255, 0, 0), -1)
       mp_drawing.draw_landmarks(
           image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
   cv2.imshow('MediaPipe Hands', image)
