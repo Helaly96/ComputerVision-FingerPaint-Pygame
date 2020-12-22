f98c77f8e88a4d759875b4ccbf4a249a1246f410
Helaly96 <zfaesu@gmail.com>
2020-12-22 22:38:48 +0200

    Tweaked API Params

--- HandPipeLine.py
+++ HandPipeLine.py
@@ -7,7 +7,7 @@ mp_hands = mp.solutions.hands
 ImageCounter= 0
 # For webcam input:
 hands = mp_hands.Hands(
-    min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    min_detection_confidence=0.9, min_tracking_confidence=0.9,max_num_hands=1)
 cap = cv2.VideoCapture(0)
 PointsToDraw= []
 while cap.isOpened():
@@ -30,7 +30,12 @@ while cap.isOpened():
   # Draw the hand annotations on the image.
   image.flags.writeable = True
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+  IndexPoints= []
+  MiddleFingerPoints= []
+
   if results.multi_hand_landmarks:
+    print(len(results.multi_hand_landmarks))
     for hand_landmarks in results.multi_hand_landmarks:
       XPixel_IndexFinger= hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * W
       YPixel_IndexFinger= hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * H
@@ -42,9 +47,11 @@ while cap.isOpened():
       cv2.circle(image, (int(XPixel_MiddleFinger),int(YPixel_MiddleFinger)), 20, (0, 0, 255), -1)
       dist= abs(np.linalg.norm(np.array([XPixel_IndexFinger,YPixel_IndexFinger])-np.array([XPixel_MiddleFinger,YPixel_MiddleFinger])))
 
-      if (dist<90):
+      if (dist<60):
         PointsToDraw.append([int(XPixel_MiddleFinger),int(YPixel_MiddleFinger)])
       else:
+        cv2.imwrite(str(ImageCounter)+".jpg",TempImage)
+        ImageCounter+=1
         PointsToDraw=[]
 
       for i in range(len(PointsToDraw)):
@@ -53,14 +60,13 @@ while cap.isOpened():
       # print("Distance between the 2 fingers"+str(dist))
       mp_drawing.draw_landmarks(
           image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
   cv2.imshow('MediaPipe Hands', image)
   cv2.imshow('Canvas', TempImage)
   k= cv2.waitKey(27)
   if k & 0xFF == 27:
     break
-  elif k == ord('e'):
-    cv2.imwrite(str(ImageCounter)+".jpg",TempImage)
-    print("!!")
+
 
 hands.close()
 cap.release()
